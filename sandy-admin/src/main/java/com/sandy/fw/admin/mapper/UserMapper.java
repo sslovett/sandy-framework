a9c22f1498632437b7f3eaa902ad0af296b9577f
huangshan <hs@cdqcp.com>
2024-03-19 14:59:59 +0800

    first commit

---
+++ sandy-admin/src/main/java/com/sandy/fw/admin/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.sandy.fw.admin.mapper;
+
+import com.sandy.fw.admin.models.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Administrator
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2024-03-19 10:59:28
+* @Entity com.sandy.fw.admin.models.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ sandy-admin/src/main/java/com/sandy/fw/admin/service/UserService.java
@@ -0,0 +1,13 @@
+package com.sandy.fw.admin.service;
+
+import com.sandy.fw.admin.models.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author Administrator
+* @description 针对表【user】的数据库操作Service
+* @createDate 2024-03-19 10:59:28
+*/
+public interface UserService extends IService<User> {
+
+}
